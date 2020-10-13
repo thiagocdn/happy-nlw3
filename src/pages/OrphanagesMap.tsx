c989e08e333925cf042970bbf422b0489aebc898
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-10-12 21:39:54 -0300

    Orphanages page styled

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import '../styles/pages/orphanage-map.css';
+
+import mapMarker from '../images/map-marker.svg';
 
 function OrphanagesMap () {
   return(
-    <h1>Hello World</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarker} alt="Map Marker"/>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Valinhos</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to='' className='create-orphanage'>
+        <FiPlus size={32} color='#FFF'/>
+      </Link>
+    </div>
   );
 };
 
